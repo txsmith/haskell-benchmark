d676d094687163db48dfd1a790eeb0e6045911e8
Thomas Smith <tnsmith@live.nl>
2018-02-04 16:56:51 +0100

    OCD

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -48,20 +48,16 @@ chopSpecialized :: Int -> Ptr Word8 -> Word64 -> IO ()
 chopSpecialized 0 _ _     = pure ()
 chopSpecialized 1 final w = poke final (asWord8 w)
 chopSpecialized 2 final w = poke (castPtr final) (asWord16 w)
-chopSpecialized 3 final w = do
-                              chopSpecialized 2 final w
-                              chopSpecialized 1 final (shiftR w 16)
+chopSpecialized 3 final w = chopSpecialized 2 final w >>
+                            chopSpecialized 1 final (shiftR w 16)
 chopSpecialized 4 final w = poke (castPtr final) (asWord32 w)
-chopSpecialized 5 final w = do
-                              chopSpecialized 4 final w
-                              chopSpecialized 1 final (shiftR w 32)
-chopSpecialized 6 final w = do
-                              chopSpecialized 4 final w
-                              chopSpecialized 2 final (shiftR w 32)
-chopSpecialized 7 final w = do
-                              chopSpecialized 4 final w
-                              chopSpecialized 2 final (shiftR w 32)
-                              chopSpecialized 1 final (shiftR w 48)
+chopSpecialized 5 final w = chopSpecialized 4 final w >>
+                            chopSpecialized 1 final (shiftR w 32)
+chopSpecialized 6 final w = chopSpecialized 4 final w >>
+                            chopSpecialized 2 final (shiftR w 32)
+chopSpecialized 7 final w = chopSpecialized 4 final w >>
+                            chopSpecialized 2 final (shiftR w 32)
+                            chopSpecialized 1 final (shiftR w 48)
 chopSpecialized 8 final w = poke (castPtr final) w
 chopSpecialized _ _ _ = error "Cannot get more than 8 bytes out of a Word64"
 
